7469cdfa88e77d9499824851235cef5ac5f038bd
Sruthias24 <sruthiasreedharan@gmail.com>
2025-01-28 20:26:02 +0530

    portfolio

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ function App() {
         <ScrollToTop />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/personal-portfolio" element={<Home />} />
+          <Route path="/personalportfolio-react" element={<Home />} />
           <Route path="/timeline" element={<Timeline/>} />
           <Route path="/education" element={<Education/>} />
           <Route path="/project" element={<Projects />} />
