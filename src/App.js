026e65188a82570d6fb5b2eacf43a02034302f41
Sruthias24 <sruthiasreedharan@gmail.com>
2025-01-29 13:12:01 +0530

    changed

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ function App() {
         <ScrollToTop />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/personalportfolio-react" element={<Home />} />
+          <Route path="/portfolio-react" element={<Home />} />
           <Route path="/timeline" element={<Timeline/>} />
           <Route path="/education" element={<Education/>} />
           <Route path="/project" element={<Projects />} />
